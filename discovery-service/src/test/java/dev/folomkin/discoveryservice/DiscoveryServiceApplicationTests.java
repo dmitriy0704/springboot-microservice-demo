f7bd398c31c37d5908c27758732aa42c0faf8239
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2026-01-31 20:11:33 +0300

    init app

---
+++ discovery-service/src/test/java/dev/folomkin/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.folomkin.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
